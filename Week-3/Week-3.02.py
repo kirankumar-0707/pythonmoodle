a2a672d501e8fb840b4e73651179ae7b15866460
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 20:40:00 +0530

    Week-3.02.py

--- Week-3/Week-3.02.py
+++ Week-3/Week-3.02.py
@@ -1,4 +1,4 @@
-Classifying Triangles
+ Classifying Triangles
 
 A triangle can be classified based on the lengths of its sides as equilateral, isosceles or scalene. All three sides of an equilateral triangle have the same length. An isosceles triangle has two sides that are the same length, and a third side that is a different length. If all of the sides have different lengths then the triangle is scalene.
 
