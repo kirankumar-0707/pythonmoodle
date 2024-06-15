595d1e3440d2a16e97551b6dd31741522fad078f
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 20:36:47 +0530

    Week-2.08.py

--- Week-2/Week-2.08.py
+++ Week-2/Week-2.08.py
@@ -1,4 +1,4 @@
-Troy Battle
+ Troy Battle
 
 In the 1800s, the battle of Troy was led by Hercules. He was a superstitious person. He believed that his crew can win the battle only if the total count of the weapons in hand is in multiple of 3 and the soldiers are in an even number of count. Given the total number of weapons and the soldier's count, Find whether the battle can be won or not according to Hercules's belief. If the battle can be won print True otherwise print False.
 
