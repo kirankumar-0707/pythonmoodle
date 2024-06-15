5301acf3acac39fcc06e9f3071306dbac054d89e
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 20:33:11 +0530

    Week-2.01.py

--- Week-2/Week-2.01.py
+++ Week-2/Week-2.01.py
@@ -1,4 +1,4 @@
-Widgets and Gizmos 
+ Widgets and Gizmos 
 
 An online retailer sells two products: widgets and gizmos. Each widget weighs 75 grams. Each gizmo weighs 112 grams. Write a program that reads the number of widgets and the number of gizmos from the user. Then your program should compute and display the total weight of the parts.
 
