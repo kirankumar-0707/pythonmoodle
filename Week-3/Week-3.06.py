e57c5962b3c8e1d25e83f8a65b721b31236f5fe3
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 20:41:33 +0530

    Week-3.06.py

--- Week-3/Week-3.06.py
+++ Week-3/Week-3.06.py
@@ -1,4 +1,4 @@
-Leap Year
+ Leap Year
 
 Most years have 365 days. However, the time required for the Earth to orbit the Sun is actually slightly more than that. As a result, an extra day, February 29, is included in some years to correct for this difference. Such years are referred to as leap years. The rules for determining whether or not a year is a leap year follow:
 
