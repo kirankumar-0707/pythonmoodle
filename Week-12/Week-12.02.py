01d3a52925cadad84e4a80b647130afa551288dd
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 21:06:44 +0530

    Week-12.02.py

--- Week-12/Week-12.02.py
+++ Week-12/Week-12.02.py
@@ -1,4 +1,4 @@
-Given an integer n, print true if it is a power of four. Otherwise, print false.
+ Given an integer n, print true if it is a power of four. Otherwise, print false.
 
 An integer n is a power of four, if there exists an integer x such that n == 4x.
 
