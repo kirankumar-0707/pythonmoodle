bb89af23c92d9be4f8fc0d1ace7cfcd3d52c5e3e
kirankumar-0707 <231001093@rajalakshmi.edu.in>
2024-06-15 20:35:51 +0530

    Week-2.06.py

--- Week-2/Week-2.06.py
+++ Week-2/Week-2.06.py
@@ -1,4 +1,4 @@
-Eligible to donate blood
+ Eligible to donate blood
 
 A team from the Rotract club had planned to conduct a rally to create awareness among the Coimbatore people to donate blood. They conducted the rally successfully. Many of the Coimbatore people realized it and came forward to donate their blood to nearby blood banks. The eligibility criteria for donating blood are people should be above or equal to 18 and his/ her weight should be above 40. There was a huge crowd and staff in the blood bank found it difficult to manage the crowd. So they decided to keep a system and ask the people to enter their age and weight in the system. If a person is eligible he/she will be allowed inside.
 
